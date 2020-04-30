2d07521056d003beb4752fbdef2c883b714d0b44
Panbaba9n <Goldgraalar@gmail.com>
2020-04-30 15:39:53 +0300

    to test git status for some folder via: "~/www/dev.electron-ipc-demo/"

--- src/main.js
+++ src/main.js
@@ -1,34 +1,17 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 
-const countdown = require('./countdown')
-
-// let mainWindow;
-const windows = []
+let mainWindow;
 
 app.on('ready', () => {
-    [1, 2, 3].forEach(() => {
-        let mainWindow = new BrowserWindow({
-            height: 400,
-            width: 400 
-        });
+    mainWindow = new BrowserWindow({
+        height: 400,
+        width: 900 
+    });
 
-        mainWindow.loadURL(`file://${__dirname}/countdown.html`);
+    mainWindow.loadURL(`file://${__dirname}/status.html`);
 
-        // countdown();
 
-        mainWindow.on('closed', () => {
-            mainWindow = null;
-        })
-
-        windows.push(mainWindow);
+    mainWindow.on('closed', () => {
+        mainWindow = null;
     })
 });
-
-ipcMain.on('countdown-start', () => {
-    console.log('caught it!')
-    countdown(count => {
-        windows.forEach(win => {
-            win.webContents.send('countdown', count)
-        });
-    });
-})
